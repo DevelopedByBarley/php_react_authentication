a740ba57fe39c4f6966f24ae09d126e8a9c39727
Árpád <developedbybarley@gmail.com>
2023-01-30 18:38:23 +0100

    cancellation of registration if email alredy exist

--- backend/server/src/index.php
+++ backend/server/src/index.php
@@ -1,64 +1,89 @@
 <?php
-  $method = $_SERVER["REQUEST_METHOD"];
-  $parsed = parse_url($_SERVER["REQUEST_URI"]);
-  $path = $parsed["path"];
-  $url = "/backend/server/src/index.php";
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+
+$method = $_SERVER["REQUEST_METHOD"];
+$parsed = parse_url($_SERVER["REQUEST_URI"]);
+$path = $parsed["path"];
+$url = "/backend/server/src/index.php";
+
+
+// Connect to the database
+require "../../db/connection.php";
+
+
+$routes = [
+  "GET" => [
+    "$url" => "homeHandler"
+  ],
+  "POST" => [
+    "$url/register" => "registrationHandler",
+    "$url/login" => "loginHandler",
+  ]
+];
+
+
+$handlerFunction = $routes[$method][$path] ?? "notFoundHandler";
+
+$handlerFunction();
+
+
+function registrationHandler()
+{
+  $_POST = json_decode(file_get_contents('php://input'), true);
+  $email = $_POST["email"];
+  $hashed = password_hash($_POST["password"], PASSWORD_DEFAULT);
   
 
-  // Connect to the database
-  require "../../db/connection.php";
-
-
-  $routes = [
-    "GET" => [
-      "$url" => "homeHandler"
-    ],
-    "POST" => [
-     "$url/registration" => "registrationHandler",
-     "$url/login" => "loginHandler",
-    ]
-  ];
-
-
-  $handlerFunction = $routes[$method][$path] ?? "notFoundHandler";
-
-  $handlerFunction();
-
   
-  function registrationHandler() {
-    $_POST = json_decode(file_get_contents('php://input'), true);
-    $hashed = password_hash($_POST["password"], PASSWORD_DEFAULT);
-
-    $pdo = createConnection();
-    $statement = $pdo -> prepare(
-      "INSERT INTO `users` (`id`, `email`, `password`, `createdAt`) VALUES (NULL, ?, ?, ?)"
-    );
-    $statement -> execute([
-      $_POST["email"],
-      $hashed,
-      time()
-    ]);
-  }
+  $pdo = createConnection();
 
 
-  function loginHandler() {
-    echo "LOGIN";
-  }
-  
-  
-  
-  
-  
-  
-  
-  
-  function homeHandler() {
-    echo "Home";
-  }
+  $statement = $pdo -> prepare("SELECT * FROM `users` WHERE email = ?");
+  $statement -> execute([$email]);
+  $isEmailExist = $statement->fetch(PDO::FETCH_ASSOC);
+
+  if($isEmailExist) {
+    echo "Email is alredy exist!";
+    return; 
+  }  
+ 
+
+
+  $statement = $pdo->prepare(
+    "INSERT INTO `users` (`id`, `email`, `password`, `createdAt`) VALUES (NULL, ?, ?, ?)"
+  );
+  $statement->execute([
+    $email,
+    $hashed,
+    time()
+  ]);
+
+  echo "Registered!";
+}
+
+
+function loginHandler()
+{
+  echo "LOGIN";
+}
 
 
 
-  function notFoundHandler() {
 
-    echo "Route not found";
-  }
+
+
+
+
+function homeHandler()
+{
+  echo "Home";
+}
+
+
+
+function notFoundHandler()
+{
+
+  echo "Route not found";
+}
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect} from 'react';
+import axios from 'axios';
 
 function App() {
+
+
+  useEffect(() => {
+    const newUser = {
+      "email": "aasdemail@email.com",
+      "password": "1234Csa"
+    }
+
+    axios.post("http://localhost:9090/backend/server/src/index.php/register", newUser)
+      .then(res => console.log(res.data))
+      .catch((error) => {
+        console.log(error , "User registration problem!")
+      })
+  }, [])
+  
+
   return (
     <div className="App">
       <header className="App-header">
